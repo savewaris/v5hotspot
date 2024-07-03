45dccfa24d9b9db49216d64cad520ce9235beeec
savewaris <savewaris@gmail.com>
2024-07-03 13:18:33 +0700

    modify

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,11 +1,35 @@
 const path = require('path');
+const webpack = require('webpack');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js', // Replace with your entry point
+  entry: 'C:/Users/gistda/Desktop/save/v5hotspot/src/server.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+  },
+  mode: 'development',
+  resolve: {
+    fallback: {
+      "assert": require.resolve("assert/"),
+      "buffer": require.resolve("buffer/"),
+      "fs": false,
+      "os": require.resolve("os-browserify/browser"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "zlib": require.resolve("browserify-zlib"),
+      "path": require.resolve("path-browserify"),
+      "querystring": require.resolve("querystring-es3"),
+      "dns": require.resolve("dns"),
+      "net": require.resolve("net"),
+      "tls": require.resolve("tls"),
+      "vm": require.resolve("vm-browserify"),
+      "dgram": false,
+      "async_hooks": require.resolve("node-libs-browser/mock/empty"), // Use node-libs-browser for async_hooks
+    },
   },
   module: {
     rules: [
@@ -15,10 +39,24 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new NodePolyfillPlugin()
+  ],
+  node: {
+    global: true,
+    __filename: true,
+    __dirname: true,
+  },
+  externals: {
+    'pg-native': 'commonjs pg-native'
   }
 };
